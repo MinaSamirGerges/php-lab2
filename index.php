c99545631a73e592f7645599cda8c6a4ce7ee241
MinaSamir <minasamirgerguis84@gmail.com>
2023-04-01 20:41:03 +0200

    second

---
+++ index.php
@@ -0,0 +1,79 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+ </head>
+ <body>
+   
+ 
+ <?php
+
+
+// #1
+// echo nl2br("my name is : \n Mina Samir Gerges");
+
+
+
+
+// #2
+
+// #A
+// $str = "Hello World!";
+// echo $str . "<br>";
+// echo chop($str,"World!");
+// echo "<br>" ;
+
+// #B
+// $str = "Hello world!";
+// echo chunk_split($str,1,".");
+// echo "<br>" ;
+
+// #C
+// echo str_repeat("Wow",13);
+
+
+
+// #3
+// foreach( $_SERVER as $key => $value) {
+//   echo "key is $key , value is $value <br/>";
+// }
+
+
+// #4
+// $arr1 = [12, 45, 10, 25];
+
+// $Sum = array_sum($arr1);
+// $average = array_sum($arr1)/count($arr1);
+
+// echo $average ."<br>" ;
+// echo $Sum ."<br>";
+
+// rsort($arr1);
+// print_r($arr1) ;
+
+
+// #5
+// $arr2=["Sara"=>31,"John"=>41,"Walaa"=>39,"Ramy"=>40];
+
+// asort($arr2);
+// print_r($arr2) ;
+// echo "<br>";
+
+// ksort($arr2);
+// print_r($arr2) ;
+// echo "<br>";
+
+// arsort($arr2);
+// print_r($arr2) ;
+// echo "<br>";
+
+// krsort($arr2);
+// print_r($arr2) ;
+
+   ?>
+
+</body>
+ </html>
